dabcc525931f7176bfbbeda32e4fdf3e7846550d
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-03-29 17:50:37 -0300

    [feat] buscar card

--- components/Buscar.vue
+++ components/Buscar.vue
@@ -11,22 +11,42 @@
     <p v-else-if="$fetchState.error">Error while fetching cars</p>
     <div v-else class="card-wrapper">
       <div v-for="(car, index) in cars.cars" :key="index" class="card">
-        <p>
-          {{ car.id }}
-        </p>
-        <p>
-          {{ car.brand }}
-        </p>
-        <!-- <img src={{ car.image }} alt="" /> -->
-        <p>
-          {{ car.km }}
-        </p>
-        <p>
-          {{ car.name }}
-        </p>
-        <p>
-          {{ car.price }}
-        </p>
+        <img :src="require(`@/assets/images/${car.image}`)" alt="" />
+        <div class="infos-wrapper">
+          <div class="infos-card-wrapper">
+            <p>
+              <strong> Marca: &nbsp;</strong>
+            </p>
+            <p>
+              {{ car.brand }}
+            </p>
+          </div>
+          <div class="infos-card-wrapper">
+            <p>
+              <strong> Nome: &nbsp;</strong>
+            </p>
+            <p>
+              {{ car.name }}
+            </p>
+          </div>
+          <div class="infos-card-wrapper">
+            <p>
+              <strong> Km: &nbsp;</strong>
+            </p>
+            <p>
+              {{ car.km }}
+            </p>
+          </div>
+          <div class="infos-card-wrapper">
+            <p>
+              <strong> Preço: &nbsp;</strong>
+            </p>
+            <p>
+              {{ car.price }}
+            </p>
+          </div>
+          <button type="button">Entrar em contato</button>
+        </div>
       </div>
     </div>
   </div>
@@ -51,6 +71,8 @@ export default {
 </script>
 
 <style lang="sass">
+@use '/assets/scss/main'
+
 .buscar-wrapper
   margin: 0 auto
   height: auto
@@ -63,8 +85,8 @@ export default {
 
 .buscar-wrapper .card-wrapper
   display: flex
-  justify-content: flex-start
-  align-items: center
+  justify-content: center
+  align-items: flex-start
   flex-flow: wrap
   width: 100%
   margin: 0 auto
@@ -72,10 +94,55 @@ export default {
   padding: 20px 40px
 
 .buscar-wrapper .card
+  box-shadow: 2px 4px 10px rgba(0, 0, 0, .2)
+  border-radius: 12px
+  overflow: hidden
+  transition: all .1s linear
   display: flex
   justify-content: center
-  align-items: center
+  align-items: flex-start
   flex-flow: column
-  background: red
   margin: 10px
+  width: 220px
+  border: 2px solid main.$color-gray
+
+.buscar-wrapper .card img
+  width: 220px
+  height: 140px
+  object-fit: cover
+
+.buscar-wrapper .card:hover
+  transform: translate3D(0, -10px, 0)
+  opacity: 0.7
+
+.buscar-wrapper .card button
+  width: 100%
+  background: main.$color-orange
+  border-radius: 5px
+  margin-top: 10px
+  color: main.$color-white
+  padding: 10px 0
+
+.buscar-wrapper .card button:hover
+  width: 100%
+  background: red
+  border-radius: 5px
+  margin-top: 10px
+  color: main.$color-gray
+
+.buscar-wrapper .infos-card-wrapper
+  display: flex
+  justify-content: center
+  align-items: flex-start
+  flex-flow: row
+  font-size: 14px
+
+.buscar-wrapper .infos-wrapper
+  padding: 20px
+  width: 100%
+  height: auto
+  display: flex
+  justify-content: center
+  align-items: flex-start
+  flex-flow: column
 </style>
