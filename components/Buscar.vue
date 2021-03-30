2451baea4d000324098933dec9e345fe188fe876
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-03-30 20:49:05 -0300

    [feat] filter on buscar section

--- components/Buscar.vue
+++ components/Buscar.vue
@@ -10,14 +10,52 @@
     <div class="title">
       <p>Buscar</p>
     </div>
-    <div class="filtros">
-      <p>Filtros:</p>
-      <input type="search" />
+    <div class="filtros container">
+      <p>Filtre o seu próximo carro:</p>
+      <label class="form-label" for="searchValue"> Carro: </label>
+      <input
+        id="searchValue"
+        v-model="searchValue"
+        type="text"
+        class="form-field"
+        name="searchValue"
+        placeholder="Ex.: Golf"
+      />
+      <select id="select" v-model="sortBy" name="sortBy">
+        <option value="max_price">max_price</option>
+        <option value="min_price">min_price</option>
+        <option value="max_km">max_km</option>
+      </select>
+      <button class="sort-button" @click="ascending = !ascending">
+        <i v-if="ascending" class="fa fa-sort-up">a</i>
+        <i v-else class="fa fa-sort-down">b</i>
+      </button>
+      <!-- <label class="form-label" for="max_price"> Preço Max: </label>
+      <input
+        id="max_price"
+        v-model="properties.max_price"
+        class="form-field"
+        name="max_price"
+      />
+      <label class="form-label" for="name"> Preço Min: </label>
+      <input
+        id="min_price"
+        v-model="properties.min_price"
+        class="form-field"
+        name="min_price"
+      />
+      <label class="form-label" for="max_km"> Max Km: </label>
+      <input
+        id="max_km"
+        v-model="properties.max_km"
+        class="form-field"
+        name="max_km"
+      /> -->
     </div>
     <p v-if="$fetchState.pending">Loading....</p>
     <p v-else-if="$fetchState.error">Error while fetching cars.</p>
     <div v-else class="card-wrapper">
-      <div v-for="car in cars.cars" :key="car.id" class="card">
+      <div v-for="car in filteredCars" :key="car.id" class="card">
         <img :src="require(`@/assets/images/${car.image}`)" alt="" />
         <div class="infos-wrapper">
           <div class="infos-card-wrapper">
@@ -70,12 +108,15 @@ export default {
       cars: [],
       isModalVisible: false,
       selectedCar: '',
-      count: 0,
-      properties: {
-        max_price: 0,
-        min_price: 0,
-        max_km: 0,
-      },
+      searchValue: '',
+      sortBy: 'max_price',
+      ascending: true,
+      // count: 0,
+      // properties: {
+      //   max_price: 0,
+      //   min_price: 0,
+      //   max_km: 0,
+      // },
     }
   },
   async fetch() {
@@ -84,6 +125,67 @@ export default {
     )
       .then((res) => res.json())
       .catch((err) => err.message)
+  },
+  computed: {
+    filteredCars() {
+      let carName = this.cars.cars
+
+      // search name
+      if (this.searchValue !== '' && this.searchValue) {
+        carName = carName.filter((item) => {
+          return item.name
+            .toUpperCase()
+            .includes(this.searchValue.toUpperCase())
+        })
+      }
+
+      // search max_price
+      if (this.max_price)
+        carName = carName.filter((item) => {
+          return item.price <= this.max_price
+        })
+
+      // search min_price
+      if (this.min_price)
+        carName = carName.filter((item) => {
+          return item.price <= this.min_price
+        })
+
+      // search max_km
+      if (this.max_km)
+        carName = carName.filter((item) => {
+          return item.km <= this.max_km
+        })
+
+      // Sort by alphabetical order
+      carName = carName.sort((a, b) => {
+        if (this.sortBy === 'max_price') {
+          const fa = a.name.toLowerCase()
+          const fb = b.name.toLowerCase()
+
+          if (fa < fb) {
+            return -1
+          }
+          if (fa > fb) {
+            return 1
+          }
+          return 0
+
+          // Sort by cooking time
+        } else if (this.sortBy === 'max_km') {
+          return a.max_km - b.max_km
+        }
+
+        return carName
+      })
+
+      // Show sorted array in descending or ascending order
+      if (!this.ascending) {
+        carName.reverse()
+      }
+
+      return carName
+    },
   },
   // mounted() {
   //   this.$notifier.showMessage({
@@ -178,4 +280,14 @@ export default {
   justify-content: center
   align-items: flex-start
   flex-flow: column
+
+.sort-button
+  background-color: rgba(0,0,0,0)
+  border: none
+  // height: 50px
+  height: 100%
+  width: 50px
+  &:hover
+    background-color: rgba(0,0,0, 0.2)
+    cursor: pointer
 </style>
