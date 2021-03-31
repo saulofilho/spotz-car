d2cb3eeb47b3a3772748dcfbb9805666e66f36a9
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-03-31 09:12:59 -0300

    [core] remove snackbar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,6 @@
     <Header />
     <Nuxt />
     <Footer />
-    <Snackbar />
   </div>
 </template>
 
--- plugins/notifier.js
+++
@@ -1,7 +0,0 @@
-export default ({ store }, inject) => {
-  inject('notifier', {
-    showMessage({ content = '', color = '' }) {
-      store.commit('snackbar/showMessage', { content, color })
-    },
-  })
-}
